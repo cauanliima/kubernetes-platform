a9b178f80803cec914edeff7140e4261aeca4b42
root <root@cauan-testes-1.sm.imd.ufrn.br>
2025-08-17 12:15:18 -0300

    ajustes

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Cores
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+CYAN='\033[0;36m'
+NC='\033[0m' # Sem cor
+
 wait_for_app_ready() {
   local APP_NAME="$1"
   local NAMESPACE="argo"
@@ -27,20 +34,25 @@ wait_for_app_ready() {
   done
 }
 
-# Instalar dependências
+echo "Instalar dependências"
 sudo apt-get update -y
 sudo apt-get install -y curl wget tar jq
 
-# Instalar RKE2
+echo "Desabilitando swap temporariamente"
+sudo swapoff -a
+# Faz backup do fstab antes
+sudo cp /etc/fstab /etc/fstab.bak.$(date +%F-%T)
+# Comentando linhas de swap no /etc/fstab para desabilitar permanentemente
+sudo sed -i '/^[^#].*swap/ s/^/#/' /etc/fstab
+
+echo "Instalar RKE2"
 curl -sfL https://get.rke2.io | sh -
 
-# Ativar o serviço server
+echp "Ativar o serviço rke2-server"
 sudo systemctl enable rke2-server.service
 sudo systemctl start rke2-server.service
 
-
 # Instalando serviços e programas
-
 echo "Instalando o kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x kubectl
@@ -69,12 +81,12 @@ while true; do
 done
 
 
-# Configurar o kubeconfig
+echo "Configurar o kubeconfig"
 mkdir -p ~/.kube
 sudo cp /etc/rancher/rke2/rke2.yaml ~/.kube/config
 sudo chown $(id -u):$(id -g) ~/.kube/config
 
-#Configurar provisionardor de volumes
+echo "Configurar provisionardor de volumes"
 mkdir /opt/local-path-provisioner
 kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/v0.0.31/deploy/local-path-storage.yaml
 kubectl patch storageclass local-path -p '{"metadata": {"annotations":{"storageclass.kubernetes.io/is-default-class":"true"}}}'
@@ -83,12 +95,7 @@ echo "Instalando ArgoCD no cluster"
 helm dependency build argocd
 helm upgrade -i argocd -n argo ./argocd --create-namespace --wait --timeout 5m
 
-# Adicionar chart do dotnet-k8s-math no chartmuseum
-helm plugin install https://github.com/chartmuseum/helm-push.git
-helm repo add --username admin --password cauan@123 chartmuseum http://localhost:32180
-helm cm-push dotnet-k8s-math/chart chartmuseum
 
-#Instalar serviços no cluster
 echo "Instalando serviços no cluster"
 DIRETORIO=("services")
 for dir in "${DIRETORIO[@]}"; do
@@ -105,15 +112,19 @@ for dir in "${DIRETORIO[@]}"; do
   fi
 done
 
-# Configurar intrumentation de serviços
+echo "Adicionar chart do dotnet-k8s-math no chartmuseum"
+wait_for_app_ready "chartmuseum"
+helm plugin install https://github.com/chartmuseum/helm-push.git
+helm repo add --username admin --password cauan@123 chartmuseum http://localhost:32180
+helm cm-push dotnet-k8s-math/chart chartmuseum -f
+
+echo "Configurar intrumentation de serviços"
 wait_for_app_ready "cert-manager"
-kubectl apply -f manifests/intrumentation.yaml
+kubectl apply -f services/openTelemetryOperator/service.yaml
+wait_for_app_ready "opentelemetry-operator"
+kubectl apply -f services/openTelemetryOperator/intrumentation.yaml
 
-#reiniciar serviços que usam instrumentação
-kubectl rollout restart deployment -n python-k8s-vault
-kubectl rollout restart deployment -n dotnet-k8s-math
-
-#Configuração vault
+echo "Configurando vault"
 wait_for_app_ready "vault"
 
 VAULT_TOKEN=cauan@123
@@ -138,17 +149,49 @@ EOP
 
 vault write auth/kubernetes/role/app-python-role \
   bound_service_account_names="*" \
-  bound_service_account_namespaces=app \
+  bound_service_account_namespaces=python-k8s-vault \
   policies=app-python-policy \
   ttl=24h
 EOF
 
+echo "reiniciar serviços que usam vault, instrumentação e chartmuseum"
+kubectl rollout restart deployment -n python-k8s-vault
+kubectl rollout restart deployment -n dotnet-k8s-math
 
-#Avisos
-echo "Acesse o argocd IP:30080"
-echo "Configuração finalizada, após is serviços subirem efetue a configuração do vault e do grafana"
+# Mensagem de término
+echo -e "$GREEN✅ Configuração finalizada! $NC"
+echo -e "$YELLOW⚠️ Após os serviços subirem, efetue a configuração do Vault e do Grafana. $NC"
+echo ""
 
-echo "Cluster RKE2 server instalado com sucesso!"
-echo "Use o seguinte token nos nós workers:"
+echo -e "$CYAN🚀 Cluster RKE2 server instalado com sucesso! $NC"
+echo -e "Use o seguinte token nos nós workers:"
 sudo cat /var/lib/rancher/rke2/server/node-token
+echo ""
 
+# Pega as portas NodePort dinamicamente
+NETDATA_PORT=$(kubectl get svc netdata -n netdata -o jsonpath='{.spec.ports[0].nodePort}')
+GRAFANA_PORT=$(kubectl get svc grafana -n grafana -o jsonpath='{.spec.ports[0].nodePort}')
+
+echo -e "$GREEN✅ Serviços NodePort e credenciais: $NC"
+echo "python-app-service : http://localhost:31001"
+echo "argocd-server : http://localhost:30080"
+echo "chartmuseum : http://localhost:32180"
+echo "coroot : http://localhost:30180"
+echo -e "grafana : http://localhost:$GRAFANA_PORT"
+echo -e "netdata : http://localhost:$NETDATA_PORT"
+echo "vault : http://localhost:30280"
+echo ""
+
+echo -e "$YELLOW🔑 Credenciais adicionais: $NC"
+echo "Vault token root: cauan@123"
+echo ""
+
+# Pega senha do grafana via kubectl
+GRAFANA_PASS=$(kubectl get secret grafana -n grafana -o jsonpath="{.data.admin-password}" | base64 -d)
+echo -e "Grafana senha admin: $GRAFANA_PASS"
+echo ""
+
+echo "Chartmuseum senha admin: cauan@123"
+echo ""
+
+echo "Faça a importação do dashboard do grafana de forma manual"
--- test.sh
+++ test.sh
@@ -1,6 +1,11 @@
+# dotnet-k8s-math 
 curl -X POST http://localhost:31002/Math/multiply -H "Content-Type: application/json" -d '{"x":2,"y":3}'
 # {"result":6}
-
 curl -X POST http://localhost:31002/Math/power -H "Content-Type: application/json" -d '{"x":7,"y":300}'
-# {"result":8}
+# {"result":3.38385702007491E+253}
 
+# python-k8s-vault
+curl http://localhost:31001/teste
+# {"teste":"teste"}
+curl http://localhost:31001/teste2
+# {"detail":"Not Found"}
